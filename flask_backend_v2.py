30cf27670fe62c609eee486794b82433b302822e
kkehoe1985 <kkehoe1985@gmail.com>
2017-02-28 21:13:55 -0500

    Updates Procfile and app.run

--- flask_backend_v2.py
+++ flask_backend_v2.py
@@ -132,5 +132,7 @@ def updatePredictions():
 	return output_json
 
 
-if __name__ == "__main__":
-	app.run(debug=True)
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
