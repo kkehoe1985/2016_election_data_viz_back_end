805bcacd539e7c4e414f5b131e8457f1f6328ffb
kkehoe1985 <kkehoe1985@gmail.com>
2017-02-28 21:17:12 -0500

    Updates Flash to bind to 0.0.0.0

--- flask_backend_v2.py
+++ flask_backend_v2.py
@@ -131,8 +131,6 @@ def updatePredictions():
 	output_json = json.dumps(output_dict)
 	return output_json
 
-
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
+    # app.run(debug=True)
     app.run(host='0.0.0.0', port=port)
