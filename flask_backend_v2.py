1c68cf8b845d7735021603b3293a8cef652c1910
kkehoe1985 <kkehoe1985@gmail.com>
2017-03-07 22:12:43 -0500

    Updates slider values

--- flask_backend_v2.py
+++ flask_backend_v2.py
@@ -61,39 +61,29 @@ def update_predictions():
 	initial_data = pd.read_csv('https://raw.githubusercontent.com/kkehoe1985/ga_data_science_final_project/master/unpacked.csv')
 
 	percent_hs_only = float(request.args.get('percent_hs_only'))
-	percent_white_male = float(request.args.get('percent_white_male'))
-	percent_bachelors = float(request.args.get('percent_bachelors'))
 	population = float(request.args.get('population'))
+	percent_white_male = float(request.args.get('percent_white_male'))
 	percent_jewish = float(request.args.get('percent_jewish'))
-	density_pop = float(request.args.get('density_pop'))
-	density_housing = float(request.args.get('density_housing'))
 	percent_white_female = float(request.args.get('percent_white_female'))
+	percent_bachelors = float(request.args.get('percent_bachelors'))
+	density_housing = float(request.args.get('density_housing'))
 	percent_black_female = float(request.args.get('percent_black_female'))
 	percent_black_male = float(request.args.get('percent_black_male'))
+	density_pop = float(request.args.get('density_pop'))
 
-
-
-
-
-	initial_data["WHITE_MALE_rate"] = initial_data['WHITE_MALE_rate'] * percent_white_male
 	initial_data["Percent of adults with a high school diploma only, 2010-2014"] = initial_data["Percent of adults with a high school diploma only, 2010-2014"] * percent_hs_only
-	initial_data["Percent of adults with a bachelor's degree or higher, 2010-2014"] = initial_data["Percent of adults with a bachelor's degree or higher, 2010-2014"] * percent_bachelors
+	initial_data["Population"] = initial_data["Population"] * population
+	initial_data["WHITE_MALE_rate"] = initial_data['WHITE_MALE_rate'] * percent_white_male
 	initial_data["Jewish"] = initial_data["Jewish"] * percent_jewish
-	initial_data["Density per square mile of land area - Population"] = initial_data["Density per square mile of land area - Population"] * density_pop
-	initial_data["Density per square mile of land area - Housing units"] = initial_data["Density per square mile of land area - Housing units"] * density_housing
 	initial_data["WHITE_FEMALE_rate"] = initial_data["WHITE_FEMALE_rate"] * percent_white_female
+	initial_data["Percent of adults with a bachelor's degree or higher, 2010-2014"] = initial_data["Percent of adults with a bachelor's degree or higher, 2010-2014"] * percent_bachelors
+	initial_data["Density per square mile of land area - Housing units"] = initial_data["Density per square mile of land area - Housing units"] * density_housing
 	initial_data["BLACK_FEMALE_rate"] = initial_data["BLACK_FEMALE_rate"] * percent_black_female
 	initial_data["BLACK_MALE_rate"] = initial_data["BLACK_MALE_rate"] * percent_black_male
-	initial_data["Population"] = initial_data["Population"] * population
-
+	initial_data["Density per square mile of land area - Population"] = initial_data["Density per square mile of land area - Population"] * density_pop
 
 	drop_columns = ['Democrat']
 	new_X = initial_data.drop(drop_columns, axis=1)
-
-	#initial_data_df = pd.DataFrame(initial_data)
-	#initial_data_dict = initial_data_df.astype(object).to_dict(orient='records')
-	#initial_data_json = json.dumps(initial_data_dict)
-	#return initial_data_json
 
 	initial_data['new_prediction'] = rf_classifier_model.predict(new_X) * 100
 	output = initial_data[['id', 'new_prediction']]
